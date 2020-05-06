fa712adc2819e26ffb2b7ae6eb93b611a8a728f8
ayushman singh gehlot <ayushmangehlot8@gmail.com>
2020-05-06 15:21:48 +0530

    modified

--- programmingtask7.go
+++ programmingtask7.go
@@ -26,10 +26,29 @@ func printInfo(perobj *person) {
 		fmt.Println(add)
 	}
 }
+
+// this a method to type person
+func (t *person) getFullName() string {
+	fullName := t.firstName + " " + t.lastName
+	return fullName
+}
+
+// since this function gets arguments as pointer its changes will be reflected to the orginal value
+func modifyPointer(perobj *person) {
+	perobj.lastName = ""
+}
+
+// since this function gets arguments as value its changes will NOT be reflected to the orginal value
+// and would be confined to the function variables(copy of original value )
+func modifyValue(perobj person) {
+	perobj.firstName = ""
+}
+
 func main() {
 
-	person1 := createPerson("pravin N S ", "gehlot", 56, []string{"R-139 Mahalaxmi Nagar", "605 rishab regency"})
+	person1 := createPerson("pravin N S", "gehlot", 56, []string{"R-139 Mahalaxmi Nagar", "605 rishab regency"})
 	printInfo(person1) // person1 itself is a pointer to struct so no need to put "&" in front of it
+	fmt.Println(person1.getFullName())
 
 	person2 := person{
 		firstName: "ayushman",
@@ -41,4 +60,10 @@ func main() {
 		},
 	}
 	printInfo(&person2)
+	fmt.Println(person2.getFullName())
+
+	modifyPointer(person1)
+	printInfo(person1)
+	modifyValue(*person1)
+	printInfo(person1)
 }
